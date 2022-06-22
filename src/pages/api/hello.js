2147366187a6a5706f1fd36eb7bd74513acf11c7
Furqon Wilogo <fdwilogo@gmail.com>
2022-06-22 22:33:19 +0700

    commit

--- src/pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from 'next'
-
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}

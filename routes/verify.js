f438a05599fff93a8431667eac9d6ef018ceeba8
Furqon Wilogo <69423972+fdwilogo@users.noreply.github.com>
2022-06-22 13:12:39 +0700

    fix error

--- routes/verify.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const verifyController = require('../src/core/helper/VerifyController');
-
-router.get('/getcode', verifyController.getCode);
-router.get('/verifycode', verifyController.verifyCode);
-
-module.exports = router;
---
+++ src/pages/api/getcode.js
@@ -0,0 +1,21 @@
+import twilio from "twilio";
+import "dotenv/config";
+
+const { ACCOUNT_SID: account_sid, AUTH_TOKEN: auth_token } = process.env;
+
+const client = twilio(account_sid, auth_token);
+
+const locale = "en";
+
+export default function handler(req, res) {
+  client.verify
+    .services(process.env.VERIFY_SERVICE_SID)
+    .verifications.create({
+      to: `+${req.query.ver}`,
+      channel: "sms",
+      locale: locale
+    })
+    .then((data) => {
+      res.status(200).send(data);
+    });
+}

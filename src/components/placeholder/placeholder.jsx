2ed7165a2194a1bb0acb66bc0c84c4adfa573a4f
Aldi Megantara Arifin <aldimegantaraa@gmail.com>
2022-06-28 16:42:09 +0700

    fix placeholder

--- src/components/placeholder/placeholder.jsx
+++ src/components/placeholder/placeholder.jsx
@@ -34,7 +34,8 @@ const Placeholder = ({}) => {
 				},
 			})
 			.then((res) => {
-				setFlip("flip-card");
+				setFlip("flip-card")
+        setBorder("border-outline")
         setSeconds(10)
         setisVerify(false)
 				return setData(res.data.results);
@@ -143,21 +144,21 @@ const Placeholder = ({}) => {
 					</div>
 				</div>
 			</div>
-      {isInvalid
-        ?
-          <>
-          </>
-        :
-          <>
-          <h4 className="text-rose-600 text-left text-sm">Invalid code! Please input the right one</h4>
-          </>
-      }
       {isVerify
           ? <>
           <h4 className="text-black text-left text-sm">{en.section1.policy} <span className="text-primary"><Link href="/privacy">Privacy Policy</Link></span></h4>
           </>
           : 
           <>
+          {isInvalid
+            ?
+              <>
+              </>
+            :
+              <>
+              <h4 className="text-rose-600 text-left text-sm">Invalid code! Please input the right one</h4>
+              </>
+          }
           <h4 className="text-black text-left text-sm">{resendTxt} <span className="text-primary">{miliseconds}{seconds}</span></h4>
           <h4 className="text-black text-left text-sm">Entering the wrong number? <span className="text-primary"><button type="button" onClick={retypeNumber}>re-type phone number</button></span></h4>
           </>

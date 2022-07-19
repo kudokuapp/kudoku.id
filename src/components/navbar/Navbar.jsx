cfee8534f95446696795e6c124a3c719fa2f9e42
Furqon Wilogo <fdwilogo@gmail.com>
2022-07-19 13:44:30 +0300

    😅  add prettier, styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,35 +1,35 @@
-import React, { useState } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import Logo from "../../../public/static/assets/logo/variant2.svg";
-import { MaterialUISwitch } from "./MaterialUISwitch";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '../../../public/static/assets/logo/variant2.svg';
+import { MaterialUISwitch } from './MaterialUISwitch';
 
-var language = "en";
+var language = 'en';
 
 export default function Navbar({}) {
-	const [navbarOpen, setNavbarOpen] = useState(false);
-	const [toggle, setToggle] = useState(true);
+  // const [navbarOpen, setNavbarOpen] = useState(false);
+  // const [toggle, setToggle] = useState(true);
 
-	const changeLanguage = (e, toggle) => {
-		if (toggle === true) {
-			language = "en";
-			localStorage.setItem("lang", language);
-		}
-		if (toggle === false) {
-			language = "id";
-			localStorage.setItem("lang", language);
-		}
-	};
-	return (
-		<nav className="navbar">
-			<div className="navbar__image cursor-pointer">
-				<Link href="/">
-					<Image src={Logo} alt="Kudoku Logo" />
-				</Link>
-			</div>
-			<div className="navbar__switch">
-				<MaterialUISwitch disabled defaultChecked />
-			</div>
-		</nav>
-	);
+  // const changeLanguage = (e, toggle) => {
+  // 	if (toggle === true) {
+  // 		language = "en";
+  // 		localStorage.setItem("lang", language);
+  // 	}
+  // 	if (toggle === false) {
+  // 		language = "id";
+  // 		localStorage.setItem("lang", language);
+  // 	}
+  // };
+  return (
+    <nav className="navbar">
+      <div className="navbar__image cursor-pointer">
+        <Link href="/">
+          <Image src={Logo} alt="Kudoku Logo" />
+        </Link>
+      </div>
+      <div className="navbar__switch">
+        <MaterialUISwitch disabled defaultChecked />
+      </div>
+    </nav>
+  );
 }
--- src/pages/imprint/index.jsx
+++ src/pages/imprint/index.jsx
@@ -1,43 +1,45 @@
-import Link from "next/link";
-import Navbar from "../../components/navbar/Navbar";
-import Sidebar from "../../components/sidebar/Sidebar";
+import Link from 'next/link';
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
 
 export default function Index() {
-	return (
-		<div className="flex md:flex-none">
-			<div className="w-0 lg:w-1/6 md:w-1/6 scale-0 sm:scale-100">
-				<Sidebar />
-			</div>
-			<div className="w-screen sm:w-5/6">
-				<Navbar />
-				<div className="bg-white p-14 rounded-3xl drop-shadow-md mt-12 mx-6">
-					<h1 className="text-primary text-6xl font-bold mb-12">Imprint</h1>
-					<h2 className="text-primary text-3xl font-medium mb-12">
-						PT. Kudoku Finansial Indonesia
-					</h2>
-					<h6 className="text-primary text-2xl">Address</h6>
-					<p className="mb-4">
-						Bellezza BSA 1st Floor Unit 106, Letjen Soepeno Street, NU/CU:
-						004/002, Urban Village: Grogol Utara, Sub-District: Kebayoran Lama,
-						District: Jakarta Selatan, Province: DKI Jakarta, Post Code: 12210
-					</p>
+  return (
+    <div className="flex md:flex-none">
+      <div className="lg:w-1/6 md:w-1/6 sm:block hidden pl-4">
+        <Sidebar />
+      </div>
+      <div className="w-screen sm:w-5/6">
+        <Navbar />
+        <section className="h-max lg:mx-6 md:mb-16 mb-8 mt-8">
+          <div className="card-main bg-onPrimary bg-opacity-70 lg:px-16 lg:py-14 sm:px-12 sm:py-10 px-8 py-8">
+            <h1 className="text-primary sm:text-6xl text-4xl font-bold mb-12">Imprint</h1>
+            <h2 className="text-primary sm:text-3xl text-2xl font-medium mb-12">
+              PT. Kudoku Finansial Indonesia
+            </h2>
+            <h6 className="text-primary text-2xl">Address</h6>
+            <p className="mb-4">
+              Bellezza BSA 1st Floor Unit 106, Letjen Soepeno Street, NU/CU: 004/002, Urban Village:
+              Grogol Utara, Sub-District: Kebayoran Lama, District: Jakarta Selatan, Province: DKI
+              Jakarta, Post Code: 12210
+            </p>
 
-					<h6 className="text-primary text-2xl">Company Registration Number</h6>
-					<p className="mb-4">AHU-0215866.AH.01.11</p>
+            <h6 className="text-primary text-2xl">Company Registration Number</h6>
+            <p className="mb-4">AHU-0215866.AH.01.11</p>
 
-					<h6 className="text-primary text-2xl">VAT-ID</h6>
-					<p className="mb-4">53.534.858.5-013.000</p>
+            <h6 className="text-primary text-2xl">VAT-ID</h6>
+            <p className="mb-4">53.534.858.5-013.000</p>
 
-					<h6 className="text-primary text-2xl">Managing Director</h6>
-					<p className="mb-4">Furqon Wilogo</p>
+            <h6 className="text-primary text-2xl">Managing Director</h6>
+            <p className="mb-4">Furqon Wilogo</p>
 
-					<h6 className="text-primary text-2xl">Managing Commissioner</h6>
-					<p className="mb-4">Prof. Dr. dr. Fachmi Idris, M.Kes</p>
+            <h6 className="text-primary text-2xl">Managing Commissioner</h6>
+            <p className="mb-4">Prof. Dr. dr. Fachmi Idris, M.Kes</p>
 
-					<h6 className="text-primary text-2xl">Company e-mail</h6>
-					<Link href="mailto:business@kudoku.id">business@kudoku.id</Link>
-				</div>
-			</div>
-		</div>
-	);
+            <h6 className="text-primary text-2xl">Company e-mail</h6>
+            <Link href="mailto:business@kudoku.id">business@kudoku.id</Link>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 }
